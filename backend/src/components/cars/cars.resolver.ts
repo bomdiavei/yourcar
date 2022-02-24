94109f875ddb0d56eff3a707570b9376d667499f
bomdiavei <bomdiavei@hotmail.com>
2022-02-24 01:16:20 -0400

    Setting up Apollo GraphQL with Nestjs

---
+++ backend/src/components/cars/cars.resolver.ts
@@ -0,0 +1,16 @@
+import { Query, Resolver } from "@nestjs/graphql";
+import { CarsService } from "./cars.service";
+
+@Resolver()
+
+export class CarsResolver {
+    constructor(private carsService: CarsService) {
+
+    }
+
+    @Query(returns => String)
+
+    public async cars() {
+        return "Hello and welcome to Yourcar shop!"
+    }
+}
