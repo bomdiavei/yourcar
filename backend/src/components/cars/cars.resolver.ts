1e0ac69af4639662e74555126d1f57cfbb53b8cb
bomdiavei <bomdiavei@hotmail.com>
2022-02-25 20:58:10 -0400

    Backend setup, and adding and fetching data

--- backend/src/components/cars/cars.resolver.ts
+++ backend/src/components/cars/cars.resolver.ts
@@ -1,16 +1,26 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
+import { NewCarInput } from "./dto/new-car.input";
+import { Car } from "./entities/car";
 
 @Resolver()
 
 export class CarsResolver {
-    constructor(private carsService: CarsService) {
+    constructor(private carsService: CarsService) { }
 
+    @Query(returns => [Car])
+    public async cars(): Promise<Car[]> {
+        return await this.carsService.getAllCars().catch((err) => {
+            throw err
+        })
     }
 
-    @Query(returns => String)
-
-    public async cars() {
-        return "Hello and welcome to Yourcar shop!"
+    @Mutation((returns) => Car)
+    public async addNewCar(
+        @Args("newCarData") newCarData: NewCarInput
+    ): Promise<Car> {
+        return await this.carsService.addCar(newCarData).catch(err => {
+            throw err
+        })
     }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// 05:54:13
+
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 9000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  app.enableCors()
+
+  await app.listen(process.env.PORT || 9000)
 }
-bootstrap();
+bootstrap()
